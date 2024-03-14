ee28fb217a2bc3a55ca1f20d1b5be66aebeb9377
saikatCV <saikat030703@gmail.com>
2024-03-14 14:19:50 +0530

    implemented uc2

---
+++ Employee/Class2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    public class Class2
+    {
+        public static void dailywage()
+        {
+            int wage_per_hour = Convert.ToInt32(Console.ReadLine());
+            int per_day_work = Convert.ToInt32(Console.ReadLine()); 
+
+            Console.WriteLine($"the wage per day is {wage_per_hour* per_day_work}")
+
+        }
+    }
+}
